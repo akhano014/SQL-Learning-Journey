f9f0c691d15cbda479b4a1071b14ee5a50d2ab38
Ahtisham khan <09-131242-014@student.bahria.edu.pk>
2025-11-23 16:07:43 +0500

    DAY 7 NOT NULL CONSTRAINTS
    In Day 7 I have clear my concept for NOT NULL constraints in SQL

---
+++ DAY 7 SQL Learning Journey.sql
@@ -0,0 +1,13 @@
+USE practice;
+CREATE TABLE Skills(
+SkillName VARCHAR(50),
+SkillsLevel INT NOT NULL,
+TimeRecquired VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Skills
+(SkillName,SkillsLevel,TimeRecquired)
+VALUES
+("Python",NULL,"1 Month");
+
+SELECT * FROM SKills;
